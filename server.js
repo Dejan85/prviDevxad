ef56178925dd5c3e9c9507e2bb4773e2f5510728
xad <xypnotica@gmail.com>
2018-11-11 09:52:11 +0100

    First commit

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-// Server static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
-}
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+//create server
+app.listen(PORT, () => {
+  console.log("Server us up");
+});
