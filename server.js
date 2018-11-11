cab40ffe19f42d68a9a31c5317d1e9706baf0a62
xad <xypnotica@gmail.com>
2018-11-11 10:11:08 +0100

    xad

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", async (req, res) => {
-  res.render("index");
-});
+app.use(express.static("public"));
 
 //create server
 app.listen(PORT, () => {
