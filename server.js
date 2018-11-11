dc8813d10bbd8d6703cf7b80a2518ed99eb4ab12
xad <xypnotica@gmail.com>
2018-11-11 09:54:44 +0100

    First commit

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/", async (req, res) => {
+  res.render("index");
+});
+
 //create server
 app.listen(PORT, () => {
   console.log("Server us up");
