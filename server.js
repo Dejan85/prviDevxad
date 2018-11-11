e71255581001872c9f64334faec55b8f7e0491e5
xad <xypnotica@gmail.com>
2018-11-11 10:20:53 +0100

    Deploy takeoff!

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const path = require("path");
 
 app.use(express.static("public"));
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 //create server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server us up");
 });
